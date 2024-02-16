5cb1387fda9826f3ae01e78b5c7beb924c67ffb3
Kuba Sobecki <gosustyle@gmail.com>
2024-02-16 11:18:11 +0100

    Initialize Next js app with shadcn UI

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-6xl text-green-500">Sup Dawg?!</h1>;
+      <Button>Look, it's a button!</Button>
+    </>
+  );
+}
