5015c6d20bc6a93fb605dbeffcb872d9479aa73e
Kuba Sobecki <gosustyle@gmail.com>
2024-03-05 21:28:35 +0100

    Add prisma generate to package.json

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button'
 
 export const LandingNavbar = () => {
   const { isSignedIn } = useAuth()
+
   return (
     <nav className="p-4 bg-transparent flex items-center justify-between">
       <Link href="/" className="flex items-center">
